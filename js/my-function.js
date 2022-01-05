21f2da217b0f6346ce8650606a79908ec37027b1
pam7464 <74638378+pam7464@users.noreply.github.com>
2022-01-05 16:30:52 +0900

    Add files via upload

--- js/my-function.js
+++ js/my-function.js
@@ -6,12 +6,29 @@ function fnFlexItemWidth(){
 }
 
 function loadEvent(leftBox,rightBox){    
-    leftBox.css({
+    $(leftBox).css({
         transform:'translateX(0)',
         opacity:'1',
     })
-    rightBox.css({
+    $(rightBox).css({
         transform:'translateX(0)',
         opacity:'1',
     })
+}
+
+function loadEventS(item1,item2,item3,item4,item5,item6,deTime){
+    setTimeout(function(){ 
+        console.log(item1,item2);       
+        loadEvent(item1,item2);
+    },deTime);
+    
+    setTimeout(function(){
+        console.log(item3,item4);           
+        loadEvent(item3,item4);
+    },deTime+600);
+    
+    setTimeout(function(){ 
+        console.log(item5,item6);          
+        loadEvent(item5,item6);
+    },deTime+1200);
 }
