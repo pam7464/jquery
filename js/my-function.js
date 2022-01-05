ae26d7393f6a2a120bd2b33c2c1a74f37d4ebd44
pam7464 <74638378+pam7464@users.noreply.github.com>
2022-01-05 15:32:00 +0900

    Add files via upload

--- js/my-function.js
+++ js/my-function.js
@@ -5,42 +5,12 @@ function fnFlexItemWidth(){
     setBox.css({width:setWidth,});
 }
 
-function loadEvent1(){
-    let sec1_1 = $('.sec01 .box01');
-    let sec1_2 = $('.sec01 .box02');
-    sec1_1.css({
+function loadEvent(leftBox,rightBox){    
+    leftBox.css({
         transform:'translateX(0)',
         opacity:'1',
     })
-    sec1_2.css({
-        transform:'translateX(0)',
-        opacity:'1',
-    })
-
-}
-
-function loadEvent2(){
-    let sec1_3 = $('.sec01 .box03');
-    let sec1_4 = $('.sec01 .box04');
-    sec1_3.css({
-        transform:'translateX(0)',
-        opacity:'1',
-    })
-    sec1_4.css({
-        transform:'translateX(0)',
-        opacity:'1',
-    })
-
-}
-
-function loadEvent3(){
-    let sec1_5 = $('.sec01 .box05');
-    let sec1_6 = $('.sec01 .box06');
-    sec1_5.css({
-        transform:'translateX(0)',
-        opacity:'1',
-    })
-    sec1_6.css({
+    rightBox.css({
         transform:'translateX(0)',
         opacity:'1',
     })
